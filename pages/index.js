ed6f0cf47056b1a5c54474e11fab40627fc9c79b
RodrigVF <92620930+RodrigVF@users.noreply.github.com>
2024-10-31 00:09:28 +0000

    fix `prettier` style linting

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>🔥"Nós devemos viver uma vida sem arrependimentos."🔥 PORTGAS D. ACE (Ep.503) 🤠👆</h1>;
+  return (
+    <h1>
+      🔥"Nós devemos viver uma vida sem arrependimentos."🔥 PORTGAS D. ACE
+      (Ep.503) 🤠👆
+    </h1>
+  );
 }
 
 export default Home;
